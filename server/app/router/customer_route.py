98ff4f1631bf936eb25d82f20ed840f2839e5a23
Ranshye <sayberdragon@gmail.com>
2026-01-08 01:38:16 +0200

    added unsecure methods to customers

--- server/app/router/customer_route.py
+++ server/app/router/customer_route.py
@@ -7,7 +7,7 @@ class CustomerRouter:
     router = APIRouter(prefix="/customers", tags=["customers"])
 
     @staticmethod
-    @router.post("/", response_model=CustomerCreateRespone)
+    @router.post("/create/validated", response_model=CustomerCreateRespone)
     def create_user(body: CustomerCreateRequest):
         try:
             return CustomerRouter.controller.create_customer(body)
@@ -15,10 +15,26 @@ class CustomerRouter:
             raise HTTPException(status_code=500, detail=str(e))
         
     @staticmethod
-    @router.post("/{id}", response_model=CustomerGetByIDResponse)
+    @router.post("/create/unvalidated", response_model=CustomerCreateRespone)
+    def create_user(body: CustomerCreateRequest):
+        try:
+            return CustomerRouter.controller.create_customer_unvalidated(body)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        
+    @staticmethod
+    @router.post("/{id}/validated", response_model=CustomerGetByIDResponse)
     def create_user(body: CustomerGetByID):
         try:
             return CustomerRouter.controller.create_customer(body)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        
+    @staticmethod
+    @router.post("/{id}/unvalidated", response_model=CustomerGetByIDResponse)
+    def create_user(body: CustomerGetByID):
+        try:
+            return CustomerRouter.controller.create_customer_unvalidated(body)
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
     
